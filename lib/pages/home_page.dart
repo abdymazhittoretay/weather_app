2906aaed170de63d04f8777e11de416fa82d6de2
Toretay <abdymvzhit@gmail.com>
2024-12-07 21:18:20 +0500

    changed weat_model and weat_service to Weather pub

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:weather_app/models/weather_model.dart';
-import 'package:weather_app/services/weather_service.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,27 +12,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final WeatherService _weatherService =
-      WeatherService(apiKey: "253a1d17598d32dfc6ce7159df4ae4ee");
+  final WeatherFactory _wf = WeatherFactory("253a1d17598d32dfc6ce7159df4ae4ee");
   Weather? _weather;
 
   void _fetchWeather() async {
-    final String cityName = await _weatherService.getCurrentCity();
+    final String cityName = await getCurrentCity();
 
-    try {
-      final Weather weather = await _weatherService.getWeather(cityName);
+    _wf.currentWeatherByCityName(cityName).then((w) {
       setState(() {
-        _weather = weather;
+        _weather = w;
       });
-    } catch (e) {
-      print(e);
-    }
+    });
   }
 
   @override
   void initState() {
-    _fetchWeather();
     super.initState();
+
+    _fetchWeather();
   }
 
   @override
@@ -44,8 +41,8 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(_weather?.cityName ?? "Loading city..."),
-            _weather?.temperature != null
+            Text(_weather?.areaName ?? "Loading city..."),
+            _weather?.areaName != null
                 ? Column(
                     children: [
                       SizedBox(
@@ -60,11 +57,11 @@ class _HomePageState extends State<HomePage> {
                       SizedBox(
                         height: 20.0,
                       ),
-                      Text("${_weather?.temperature.round()}°C"),
+                      Text("${_weather?.temperature?.celsius?.toStringAsFixed(0)}°C"),
                       SizedBox(
                         height: 20.0,
                       ),
-                      Text(_weather?.mainCondition ?? "")
+                      Text(_weather?.weatherDescription ?? "")
                     ],
                   )
                 : SizedBox(),
@@ -72,5 +69,31 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
+  }
+
+  // Functions
+  Future<String> getCurrentCity() async {
+    // Checks permission
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      // Requests permission
+      permission = await Geolocator.requestPermission();
+    }
+
+    // gets the current location
+    Position position = await Geolocator.getCurrentPosition(
+        locationSettings: LocationSettings(
+      accuracy: LocationAccuracy.high,
+    ));
+
+    // converts current location to placemarks
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    // selects city name from placemarks
+    final String? city = placemarks[0].locality;
+
+    // if variable city is null it will return ""
+    return city ?? "";
   }
 }
